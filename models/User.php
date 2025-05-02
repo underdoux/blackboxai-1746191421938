e8975751571e211a8d3a982f06b3224654e76257
JustAjie <underdoux@users.noreply.github.com>
2025-05-02 20:11:05 +0700

    Add models/User.php 1746191463187

---
+++ models/User.php
@@ -0,0 +1,35 @@
+<?php
+require_once __DIR__ . '/../config/database.php';
+
+class User {
+    private $conn;
+    private $table_name = "users";
+
+    public $id;
+    public $username;
+    public $password;
+    public $role_id;
+    public $created_at;
+
+    public function __construct() {
+        $database = new Database();
+        $this->conn = $database->getConnection();
+    }
+
+    public function findByUsername($username) {
+        $query = "SELECT * FROM " . $this->table_name . " WHERE username = :username LIMIT 0,1";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':username', $username);
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function verifyPassword($username, $password) {
+        $user = $this->findByUsername($username);
+        if ($user && password_verify($password, $user['password'])) {
+            return $user;
+        }
+        return false;
+    }
+}
+?>
